15cd95d022155caec99ca4a25111b26b4a0ce377
Reborn <ren_xiaoyao@outlook.com>
2020-02-24 19:05:57 +0800

    init project

---
+++ README.md
@@ -0,0 +1,3 @@
+# LearnCustomView
+
+这是一个我用来学习 自定义View 的仓库
---
+++ app/src/main/java/com/mallotec/reb/learncustomview/MainActivity.java
@@ -0,0 +1,14 @@
+package com.mallotec.reb.learncustomview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
